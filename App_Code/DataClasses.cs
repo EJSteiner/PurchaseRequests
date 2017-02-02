c03500af089f755f19d22e772cac6ff85668b38f
EJSteiner <ellenjsteiner@gmail.com>
2017-02-02 18:19:21 -0500

    Updated to fix file not found problem with website buttons

---
+++ App_Code/DataClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PurchasingTableAdapters;
+
+/// <summary>
+/// Summary description for DataClasses
+/// </summary>
+public class DataClasses
+{
+    public DataClasses()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    
+    public Purchasing.getVendorsDataTable getVendors(string selectCriteria, string selectValue)
+    {
+        Purchasing.getVendorsDataTable dtVendors = new Purchasing.getVendorsDataTable();
+        getVendorsTableAdapter taVendors = new getVendorsTableAdapter();
+        switch (selectCriteria)
+        {
+            case "Name":
+                taVendors.FillByName(dtVendors,selectValue);
+                break;
+            case "State":
+                taVendors.FillByState(dtVendors,selectValue);
+                break;
+            default:
+                taVendors.Fill(dtVendors);
+                break;
+        }
+
+        // add HTTP:\\ to begining of each website name
+        string fullName;
+        foreach (DataRow row in dtVendors.Rows)
+        {
+            fullName = "http://" + row["Website"];
+            row["Website"] = fullName;
+        }
+
+        return dtVendors;
+    }
+
+        
+    }
